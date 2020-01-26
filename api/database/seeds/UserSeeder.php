3cb9b5963795d568b6619d8bb006a3ebcb76b83e
Remarc Balisi <remarc.balisi@fligno.com>
2020-01-26 20:37:31 +0800

    role and admin user seeder

---
+++ api/database/seeds/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        // admin
+        $admin = User::create([
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+            'username' => 'admin',
+            'remember_token' => Str::random(10),
+        ]);
+
+        $admin_role = Role::findByName('admin', 'api');
+
+        $admin->assignRole($admin_role);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -5,6 +5,17 @@ use Illuminate\Http\Request;
 //Request token
 Route::post('token', 'Api\TokenController@store')->name('request.token');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//ADMIN ROUTES
+Route::group([
+    'prefix' => 'v1/admin',
+    'namespace' => 'Api\Admin'
+], function() {
+
+    Route::group([
+        'middleware' => [
+            'auth:api'
+        ]
+    ], function () {
+
+    });
 });
